4222ff4dd51e768c5ffec217873fc2b0b7ebaec4
ESC18 <ponderosaprintingus@gmail.com>
2023-06-26 20:24:36 -0700

    Updated code - now uses promise.then, promise, resolve, reject, open, send to minimize code and also increase capability.

--- src/index.js
+++ src/index.js
@@ -4,19 +4,27 @@ import './assets/fonts.css';
 import './css/styles.css';
 //EX: import Blank from '/.js/secondary.js';
 
-function getWeather(city, state) {
-    let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${state}&appid=${process.env.API_KEY}`;
-
-    request.addEventListener("loadend", function () {
-        const response = JSON.parse(this.responseText);
-        if (this.status === 200) {
-            printElements(response, city, state);
-        }
+function getWeather(city) {
+    let promise = new Promise(function (resolve, reject) {
+        let request = new XMLHttpRequest();
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+        request.addEventListener("loadend", function () {
+            const response = JSON.parse(this.responseText);
+            if (this.status === 200) {
+                resolve([response, city]);
+            } else {
+                reject([this, response, city]);
+            }
+        });
+        request.open("GET", url, true);
+        request.send();
     });
 
-    request.open("GET", url, true);
-    request.send();
+    promise.then(function (response) {
+        printElements(response);
+    }, function (errorMessage) {
+        printError(errorMessage);
+    });
 }
 
 // UI Logic
@@ -25,6 +33,10 @@ function printElements(apiResponse, city, state) {
     document.querySelector('#showResponse').innerText = `The humidity in ${city}, ${state} is ${apiResponse.main.humidity}%.
     The temperature in Kelvins is ${apiResponse.main.temp} degrees.`;
 }
+
+function printError(error) {
+    document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${error[2]}: ${error[0].status} ${error[0].statusText}: ${error[1].message}`;
+  }
 
 function handleFormSubmission(event) {
     event.preventDefault();
